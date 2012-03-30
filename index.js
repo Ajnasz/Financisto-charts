4d7c9bcb1ad4d0c408de6afc5ab28f062fc5115d
Lajos Koszti <ajnasz@ajnasz.hu>
2012-03-30 08:12:45 +0200

    Set port for nodester

--- index.js
+++ index.js
@@ -200,5 +200,6 @@ setInterval(function () {
     console.log('memory usage: ', process.memoryUsage());
 }, 600000);
 
-http.createServer(processRequest).listen(16442, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:16442/');
+
+http.createServer(processRequest).listen(16444, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:16444/');
